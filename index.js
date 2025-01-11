cc408dd781e9ebcf86095976447164eca334dbfe
Nnisik <nisik0120@gmail.com>
2025-01-11 23:45:26 +0100

    setup webpack

--- index.js
+++
@@ -1,5 +0,0 @@
-// Theme switching
-const themeSwitchBtn = document.getElementById('theme-switch-btn');
-themeSwitchBtn.addEventListener('click', () => {
-    document.documentElement.classList.toggle('dark-theme');
-});

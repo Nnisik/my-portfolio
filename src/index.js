20055309ba2ec3a390dd27814142f3c2ae658e79
Nnisik <nisik0120@gmail.com>
2025-02-02 15:00:13 +0100

    fixed ui error

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ document.addEventListener("DOMContentLoaded", () => {
     fillSkillSection("../src/json/data-skills-front.json", "skills-front");
     fillSkillSection("../src/json/data-skills-languages.json", "skills-languages");
     fillSkillSection("../src/json/data-skills-tools.json", "skills-tools");
-    fillSkillSection("../src/json/data-skills-databases.json", "skills-databases");
+    fillSkillSection("../src/json/data-skils-databases.json", "skills-databases");
 });
